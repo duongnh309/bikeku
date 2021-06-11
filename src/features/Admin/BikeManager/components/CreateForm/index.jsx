c30b7f5b518cbbd293ee73838f9a16a618002144
vuvietsang10a9 <vuvietsang10a9@gmail.com>
2021-06-11 19:23:41 +0700

    gitUpdate

--- src/features/Admin/BikeManager/components/CreateForm/index.jsx
+++ src/features/Admin/BikeManager/components/CreateForm/index.jsx
@@ -16,14 +16,12 @@ CreateForm.propTypes = {
 
 function CreateForm(props) {
     const schema = yup.object().shape({
-        name: yup.string().required('Please enter your name!')
-            .test('should has at least two words', 'please enter at least two words', (value) => {
-                return value.split(' ').length >= 2;
-            }),
+        name: yup.string().required('Please enter your name!'),
         maker: yup.string().required('Please enter the maker!'),
         price: yup.number().min(0),
         quantity: yup.number().min(0).integer(),
         category: yup.string().required('Please enter your category!'),
+        minQuantity:yup.number().min(0).integer(),
     });
 
     const form = useForm({
@@ -32,13 +30,15 @@ function CreateForm(props) {
             maker: '',
             description: '',
             price: '',
-            quantity: '',
+            quantity: 0,
             category: '',
+            minQuantity : 0,
         },
         resolver: yupResolver(schema),
     });
 
     const handleSubmit = (Values) => {
+        console.log(Values);
         const { onSubmit } = props;
         if (onSubmit) {
             onSubmit(Values);
@@ -47,78 +47,40 @@ function CreateForm(props) {
         form.reset();
     }
 
-    const [files, setFiles] = useState([]);
-
-
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
-        accept: "image/*",
-        onDrop: (acceptedFiles) => {
-            setFiles(
-                acceptedFiles.map((file) =>
-                    Object.assign(file, {
-                        preview: URL.createObjectURL(file),
-                    }))
-            )
-        }
-    })
-
-    const images = files.map((file) => (
-        <div key={file.name}>
-            <div>
-                <img src={file.preview} style={{ width: '200px',height:'150px' }} alt="preview" />
-            </div>
-            {console.log(files)}
-        </div>
-    ))
-
-    const dropzoneActive = {
-        borderColor: 'green',
+    const [image, setImage] = useState('');
+    console.log(image);
+    const onChangeImg = (event)=>{
+        if (event.target.files && event.target.files[0]) {
+            let img = event.target.files[0];
+            setImage(URL.createObjectURL(img));
+          }
     }
 
-    const dropzoneStyle = {
-        border: 'dashed 3px',
-        borderColor: '#eee',
-        borderRadius: '5px',
-        paddingTop: '30px', 
-        textAlign: 'center', 
-        height: '200px',
-        width: '200px'
-    }
-
+    const {register} = form;
     return (
         <div>
             <div>
                 <Avatar>
                     <LockOutlined>
-
                     </LockOutlined>
                 </Avatar>
             </div>
-
-
             <form onSubmit={form.handleSubmit(handleSubmit)}>
                 <InputFeild name='name' label='Name' form={form} />
                 <InputFeild name='maker' label='Maker' form={form} />
                 <InputFeild name='description' label='Description' form={form} />
                 <InputFeild name='price' label='Price' form={form} />
                 <InputFeild name='quantity' label='Quantity' form={form} />
+                <InputFeild name='minQuantity' label='MinQuantity' form={form} />
                 <InputFeild name='category' label='Category' form={form} />
-
-                <div {...getRootProps()} style={isDragActive?{...dropzoneStyle,...dropzoneActive}:dropzoneStyle}>
-                        <input {...getInputProps()} />
-                        <PublishIcon style={{ fontSize: 40 }}/>
-                        <Typography>
-                        Drop image here!
-                        </Typography>
-                    <div>{images}</div>
-                </div>
+                <Typography>Select Img : </Typography>
+                <input  {...register('imageUrl')} type="file" id="my-img" name="ImageStyle" onChange={onChangeImg} />
+                <img src={image} style={{width:'180px',height:'100px'}} />
                 <br/>
                 <br/>
-
                 <button className="btn btn-primary" type='submit' >
                     <i className="fa fa-edit"></i> Create New Product
                 </button>
-
                 <a className="btn btn-default" href="/admin/bikes" type='submit' style={{float:'right'}}> 
                     Cancel
                 </a>
