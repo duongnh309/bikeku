16037989e41e7d01963a228dfe3ad1cc2c90607e
studingHtml <hieuthomnghiep@gmail.com>
2021-06-11 10:02:09 +0700

    MaintableUpodated

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -2,51 +2,53 @@
 import { axiosClient } from "./axiosClient";
 
 const productApi = {
-    // async getAll(params) {
-    //     //Transform _page to _start
-    //     const newParams = { ...params }
-    //     newParams._start = !params._page || params._page <= 1
-    //         ? 0 : (params._page - 1) * (params._limit || 50);
-    //     //Remove un-needed key
-    //     delete newParams._page
-    //     //fetch products list +count
-    //     const productList = await easyFrontend.get('/products', {
-    //         params:
-    //             newParams
-    //     });
-    //     const count = await easyFrontend.get('/products/count', {
-    //         params:
-    //             newParams
-    //     })
-    //     return {
-    //         data: productList,
-    //         pagination: {
-    //             page: params._page,
-    //             _limit: params._limit,
-    //             total: count.data
-    //         }
-    //     }
-    // },
+    //Transform _page to _start
+    // const newParams = { ...params }
+    // newParams._start = !params._page || params._page <= 1
+    //     ? 0 : (params._page - 1) * (params._limit || 50);
+    // //Remove un-needed key
+    // delete newParams._page
+    // //fetch products list +count
+    async getAll(params) {
+        var url = '/Products';
+        if (params.searchBy === 'Maker') {
+            url = `/Products/Maker/${params.value}`;
+        }
+        else if (params.searchBy === 'Name') {
+            url = `/Products/Name/${params.value}`;
+        }
+        else if (params.priceRange) {
+            if (params.priceRange !== 'default') {
+                var [min, max] = params.priceRange.split('-');
+                if (params.priceRange === '100000') {
+                    min = 8000;
+                    max = 100000;
+                }
+                url = `/Products/Price/${min}/${max}`;
+            }
+        }
+        delete params.searchBy;
+        delete params.priceRange;
+
+        const response = await (axiosClient.get(url, {
+            params: params
+        }));
+        return {
+            data: response.data,
+            pagination: {
+                total: response.total,
+                page: params.PageNumber,
+                PageSize: params.PageSize
+            }
+        }
+    },
 
 
-    async get(id){
+    async get(id) {
         const url = `/api/products/${id}`;
         return (await axiosClient.get(url)).data;
     },
 
-    async getAll(){
-        const url = '/api/products';
-        const productList = (await axiosClient.get(url)).data;
-        console.log(productList);
-        return {
-            data:productList,
-            pagination: {
-                page:1,
-                _limit:9,
-                total:30
-            }
-        }
-    }
 
 }
 
--- src/features/Admin/BikeManager/components/MainTable/index.jsx
+++ src/features/Admin/BikeManager/components/MainTable/index.jsx
@@ -1,12 +1,11 @@
+import { Pagination } from '@material-ui/lab';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import productApi from '../../../../../api/productApi';
 
 
 MainTable.propTypes = {
-    bikes: PropTypes.array,
-    deleteABike: PropTypes.func,
-    updateABike: PropTypes.func,
 };
 
 MainTable.defaultProps = {
@@ -14,24 +13,50 @@ MainTable.defaultProps = {
 };
 
 
-function MainTable({ bikes, deleteABike }) {
+function MainTable() {
+
+    const [filter, setFilter] = useState({ PageSize: 10, PageNumber: 1 });
+    const [pagination, setPagination] = useState({total:50,page:1,PageSize:10});
+
+    useEffect(() => {
+        fetchDataFormApi();
+        // eslint-disable-next-line
+    }, [filter]);
+
+    const [bikes, setBikes] = useState([]);
+    const fetchDataFormApi = async () => {
+        const response = await productApi.getAll(filter);
+        const bikeFromApi = response.data;
+        setBikes(bikeFromApi);
+        setPagination()
+    }//fetch data base on filter
+
+    //mac dinh la pagesize 
 
     const handleDelete = (id) => {
-        if (deleteABike) {
-            deleteABike(id);
-        };
+        console.log(id);
     }
 
+    //Search sp
     const form = useForm();
     const { register, handleSubmit } = form;
     const handleSearch = (value) => {
         const searchValue = value.search;
-        console.log(searchValue);
+        const searchBy = value.searchBy;
+        setFilter({ ...filter, value: searchValue, searchBy });
     }
 
-    const handleNumberInPage = (e) =>{
+
+    //Xu ly so sp hien thi tren table
+    const handleNumberInPage = (e) => {
         const numberInPages = e.target[e.target.selectedIndex].getAttribute('value');
-        console.log(numberInPages);
+        setFilter({ ...filter, PageSize: numberInPages });
+    }
+
+    //Xu ly search theo price
+    const handlePirce = (e) =>{
+        const priceRange = e.target[e.target.selectedIndex].getAttribute('value');
+        setFilter({ ...filter, priceRange});
     }
 
 
@@ -54,37 +79,63 @@ function MainTable({ bikes, deleteABike }) {
                             <div className="row">
                                 <div className="col-sm-6">
                                     <div className="dataTables_length" id="dataTables-example_length">
-                                        <label>
+                                        <label style={{ display: 'flex', alignItems: 'center' }}>
                                             <select onChange={handleNumberInPage} name="records" aria-controls="dataTables-example" className="form-control input-sm">
                                                 <option value="10">10</option>
                                                 <option value="25">25</option>
                                                 <option value="50">50</option><option value="100">100</option>
                                             </select>
                                             <span>records per page</span>
-                                    </label>
-                                    
+                                        </label>
+
                                     </div>
                                 </div>
-                                <div className="col-sm-6">
-                                    <form onSubmit={handleSubmit(handleSearch)}>
+                                <div className="col-sm-6" >
+                                    <form onSubmit={handleSubmit(handleSearch)} style={{ marginLeft: '2em' }}>
+
                                         <div id="dataTables-example_filter" className="dataTables_filter">
-                                        <label>Search:<input type="search" className="form-control input-sm" aria-controls="dataTables-example" {...register('search')} /></label>
+                                            <label style={{ marginLeft: '2em' }}>
+                                                <select name="records" aria-controls="dataTables-example" className="form-control input-sm col-sm-4" {...register('searchBy')}>
+                                                    <option value="Maker">Maker</option>
+                                                    <option value="Name">Name</option>
+                                                </select>
+
+                                            </label>
+                                            <label style={{ display: 'flex', alignItems: 'center', marginLeft: '2em' }}>Search:<input type="search" className="form-control input-sm" aria-controls="dataTables-example" {...register('search')} /></label>
+
                                         </div>
                                     </form>
+
+                                    <div id="dataTables-example_filter" onChange={handlePirce} className="dataTables_filter">
+                                        <label>
+                                            <span style={{ margin: 'auto 0' }}>Price</span>
+                                            <select name="records" aria-controls="dataTables-example" className="form-control input-sm col-sm-4">
+                                                <option value="100000">Greater than 8000</option>
+                                                <option value="5000-8000">from 5000 to 8000</option>
+                                                <option value="1000-5000">from 1000 to 5000</option>
+                                                <option value="500-1000">from 500 to 1000</option>
+                                                <option value="100-500">from 100 to 500</option>
+                                                <option value="1-100">from 1 to 100</option>
+                                                <option value='default' selected></option>
+                                            </select>
+                                        </label>
+                                    </div>
+
 
                                 </div>
                             </div>
                             <table className="table table-striped table-bordered table-hover" id="dataTables-example">
                                 <thead>
                                     <tr>
-                                        <th>id</th>
-                                        <th>image</th>
-                                        <th>name</th>
-                                        <th>maker</th>
-                                        <th>description</th>
-                                        <th>price</th>
+                                        <th>Id</th>
+                                        <th>Image</th>
+                                        <th>Name</th>
+                                        <th>Maker</th>
+                                        <th>Description</th>
+                                        <th>Price</th>
                                         <th>Quantity</th>
                                         <th>Category</th>
+                                        <th>CreateDay</th>
                                         <th>Delete</th>
                                         <th>Update</th>
                                     </tr>
@@ -93,13 +144,14 @@ function MainTable({ bikes, deleteABike }) {
                                     {bikes.map((bike) => (
                                         <tr className="gradeU" key={bike.id}>
                                             <td>{bike.id}</td>
-                                            <td><img width="50px" height="50px" src={bike.image} className="img-fluid" alt="This a pic" /></td>
+                                            <td><img width="50px" height="50px" src={bike.imageUrl} className="img-fluid" alt="This a pic" /></td>
                                             <td>{bike.name}</td>
                                             <td>{bike.maker}</td>
                                             <td>{bike.description}</td>
                                             <td>{bike.price}</td>
                                             <td>{bike.quantity}</td>
                                             <td>{bike.category}</td>
+                                            <td>{bike.createDate}</td>
                                             <td><button onClick={() => handleDelete(bike.id)} className="btn btn-danger"><i className="fa fa-pencil"></i> Delete</button></td>
                                             <td><a href={`/admin/bikes/update?id=${bike.id}`} className="btn btn-primary"><i className="fa fa-edit "></i> Edit</a>
 
@@ -114,15 +166,7 @@ function MainTable({ bikes, deleteABike }) {
                                 </div>
                                 <div className="col-sm-6">
                                     <div className="dataTables_paginate paging_simple_numbers" id="dataTables-example_paginate">
-                                        <ul className="pagination">
-                                            <li className="paginate_button previous disabled" aria-controls="dataTables-example" tabIndex="0" id="dataTables-example_previous"><a href="#">Previous</a></li>
-                                            <li className="paginate_button active" aria-controls="dataTables-example" tabIndex="0"><a href="#">1</a></li><li className="paginate_button " aria-controls="dataTables-example" tabIndex="0"><a href="#">2</a></li>
-                                            <li className="paginate_button " aria-controls="dataTables-example" tabIndex="0"><a href="#">3</a></li>
-                                            <li className="paginate_button " aria-controls="dataTables-example" tabIndex="0"><a href="#">4</a></li>
-                                            <li className="paginate_button " aria-controls="dataTables-example" tabIndex="0"><a href="#">5</a></li>
-                                            <li className="paginate_button " aria-controls="dataTables-example" tabIndex="0"><a href="#">6</a></li>
-                                            <li className="paginate_button next" aria-controls="dataTables-example" tabIndex="0" id="dataTables-example_next"><a href="#">Next</a></li>
-                                        </ul>
+                                    
                                     </div>
                                 </div>
                             </div>
