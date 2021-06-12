a80c163728309cf7508fe3fb10e5d44531a7185c
studingHtml <hieuthomnghiep@gmail.com>
2021-06-12 11:02:00 +0700

    Update Confirm

--- src/features/Admin/OrderManager/components/OrderDetailTable/index.jsx
+++ src/features/Admin/OrderManager/components/OrderDetailTable/index.jsx
@@ -1,67 +1,69 @@
+import { useSnackbar } from 'notistack';
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import orderApi from '../../../../../api/orderApi';
 
 OrderDetailTable.propTypes = {
-    
+
 };
 
-function OrderDetailTable({order}) {
-    const {orderDetails} = order;
+function OrderDetailTable({ order }) {
+    const history = useHistory();
+    const { orderDetails } = order;
+    const { enqueueSnackbar } = useSnackbar();
     const handleConfirm = (orderId) => {
-        orderApi.confirm(orderId);
-        //xu ly sau
+        try {
+            (orderApi.confirm(orderId))
+            .then(enqueueSnackbar('Successfully', { variant: 'success' }))
+            .then(history.replace('/admin/orders'));
+        } catch (error) {
+            console.log('Confirm Failded', error);
+            enqueueSnackbar(error.message, { variant: 'error' });
+        }
     }
     return (
         <div>
             <div className="row">
-            <div className="col-md-12">
-                {/* Advanced Tables */}
-                
-                <div className="panel panel-default ">
-                    <div className="panel-heading">
-                        Bikes Managers Tables
-                    </div>
-                    <div className="panel-body">
-                        <div className="table-responsive">
-                            <table className="table table-striped table-bordered table-hover" id="dataTables-example">
-                                <thead>
-                                    <tr>
-                                        <th>Id</th>
-                                        <th>Image</th>
-                                        <th>Name</th>
-                                        <th>Maker</th>
-                                        <th>Description</th>
-                                        <th>Price</th>
-                                        <th>Quantity</th>
-                                        <th>Category</th>
-                                        <th>CreateDay</th>
-                                        <th>Delete</th>
-                                        <th>Update</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {orderDetails.map((orderDetail) => (
-                                        <tr className="gradeU" key={orderDetail.productId}>
-                                            <td>{orderDetail.productId}</td>
-                                            <td>{orderDetail.quantity}</td>
-                                            <td>{orderDetail.price}</td>
-                                            <td>{orderDetail.createDate}</td>
+                <div className="col-md-12">
+                    {/* Advanced Tables */}
+
+                    <div className="panel panel-default ">
+                        <div className="panel-heading">
+                            Order Details
+                        </div>
+                        <div className="panel-body">
+                            <div className="table-responsive">
+                                <table className="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>Product Id</th>
+                                            <th>Quantity</th>
+                                            <th>Price</th>
+                                            <th>Create Date</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                            <button onClick={() => handleConfirm(order.id)} className="btn btn-danger"><i className="fa fa-pencil"></i> Confirm</button>
+                                    </thead>
+                                    <tbody>
+                                        {orderDetails.map((orderDetail) => (
+                                            <tr className="gradeU" key={orderDetail.productId}>
+                                                <td>{orderDetail.productId}</td>
+                                                <td>{orderDetail.quantity}</td>
+                                                <td>{orderDetail.price}</td>
+                                                <td>{orderDetail.createDate}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                                <button onClick={() => handleConfirm(order.id)} className="btn btn-danger"><i className="fa fa-pencil"></i> Confirm</button>
+                            </div>
                         </div>
                     </div>
+                    {/*End Advanced Tables */}
                 </div>
-                {/*End Advanced Tables */}
+
+
+
+
             </div>
-
-
-
-
-        </div>
         </div>
     );
 }
--- src/features/Admin/OrderManager/components/OrdersTable/index.jsx
+++ src/features/Admin/OrderManager/components/OrdersTable/index.jsx
@@ -64,7 +64,7 @@ function OrdersTable() {
                                                 <select onChange={handleNumberInPage} name="records" aria-controls="dataTables-example" className="form-control input-sm">
                                                     <option value="10">10</option>
                                                     <option value="25">25</option>
-                                                    <option value="50">50</option><option value="100">100</option>
+                                                    <option value="50">50</option>
                                                 </select>
                                                 <span>records per page</span>
                                             </label>
