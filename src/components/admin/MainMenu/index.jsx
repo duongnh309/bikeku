074e945fde8aa27e49c4aadb86cfbfe62b0fe705
studingHtml <hieuthomnghiep@gmail.com>
2021-06-14 19:57:00 +0700

    Last update

--- src/components/admin/MainMenu/index.jsx
+++ src/components/admin/MainMenu/index.jsx
@@ -4,41 +4,33 @@ import classNames from 'classnames';
 import { useLocation } from 'react-router';
 
 MainMenu.propTypes = {
-    
+
 };
 
 function MainMenu(props) {
-    const location = useLocation();
-    const tab = location.pathname;
-    const user = JSON.parse(localStorage.getItem('user'));
-    return (
-        <nav className="navbar-default navbar-side" role="navigation">
-          <div className="sidebar-collapse">
-            <ul className="nav" id="main-menu" style={{display:'block'}}>
-              <li className="text-center">
-                <img src="/assets/img/find_user.png" className="user-image img-responsive" />
-                <h2 style={{color:'white'}}>{user&& user.name}</h2>
-              </li>
-              <li>
-                <a className={classNames({
-                  "active-menu":tab.includes('/admin/bikes')
-                })} href="/admin/bikes"><i className="fa fa-dashboard fa-3x" /> Bikes</a>
-              </li>
-              <li>
+  const location = useLocation();
+  const tab = location.pathname;
+  const user = JSON.parse(localStorage.getItem('user'));
+  const roles = user.role;
+  return (
+    <nav className="navbar-default navbar-side" role="navigation">
+      <div className="sidebar-collapse">
+        <ul className="nav" id="main-menu" style={{ display: 'block' }}>
+          <li className="text-center">
+            <img src="/assets/img/find_user.png" className="user-image img-responsive" />
+            <h2 style={{ color: 'white' }}>{user && user.name}</h2>
+          </li>
+          {roles.map(
+            <li>
               <a className={classNames({
-                  "active-menu":tab.includes('/admin/accounts')
-                })} href="/admin/accounts"><i className="fa fa-dashboard fa-3x" /> Accounts</a>
-              </li>
-              <li>
-              <a className={classNames({
-                  "active-menu":tab.includes('/admin/orders')
-                })} href="/admin/orders"><i className="fa fa-dashboard fa-3x" /> Orders</a>
-              </li>
+                "active-menu": tab.includes('/admin/bikes')
+              })} href="/admin/bikes"><i className="fa fa-dashboard fa-3x" /> Bikes</a>
+            </li>)}
 
-            </ul>
-          </div>
-        </nav>  
-    );
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default MainMenu;           
+export default MainMenu;
