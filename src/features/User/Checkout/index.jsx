893eb719a210aa22c6ad4f823bb28eb5455336be
nhd1123 <parkunduong@gmail.com>
2021-06-15 13:07:09 +0700

    update require when guess check their cart

--- src/features/User/Checkout/index.jsx
+++ src/features/User/Checkout/index.jsx
@@ -15,14 +15,22 @@ function CheckoutFeature(props) {
     return (
         <div>
             <Switch>
-                
-                <Route path="/checkout/cart" component={ShoppingCart} />
-                <Route path="/checkout/history" key={nanoid()} component={OrderHistory} />
-                <Route path="/checkout/history/detail" key={nanoid()} component={OrderHistory} />
-
                 <Route path="/checkout" exact render={() => {
                     return localStorage.getItem('user') ? <Redirect to="/bikes" /> : <CheckoutPage />
                 }} />
+                <Route path="/checkout/cart" exact render={() => {
+                    return localStorage.getItem('user') ? <ShoppingCart /> : <CheckoutPage />
+                }} />
+                <Route path="/checkout/history" exact render={() => {
+                    return localStorage.getItem('user') ? <OrderHistory /> : <CheckoutPage />
+                }} />
+                <Route path="/checkout/history/detail" exact render={() => {
+                    return localStorage.getItem('user') ? <OrderHistory /> : <CheckoutPage />
+                }} />
+
+                 {/* <Route path="/checkout/cart" component={ShoppingCart} />
+                <Route path="/checkout/history" key={nanoid()} component={OrderHistory} />
+                <Route path="/checkout/history/detail" key={nanoid()} component={OrderHistory} /> */}
 
             </Switch>
         </div>
